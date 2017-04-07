53fd6b2d1a5f287dbfa5daf0f87c1451b8bf00e2
Cameron Weston <cameron.weston@banno.com>
2017-04-06 21:25:10 -0500

    Added asynchronous call to fetch tweets web service
    Added JSON parsing to a dictionary of all incoming tweets in the fetch tweets method

--- TwitterStats/TwitterStats/FetchTweetsWebService.m
+++ TwitterStats/TwitterStats/FetchTweetsWebService.m
@@ -8,11 +8,36 @@
 
 #import "FetchTweetsWebService.h"
 
+@interface FetchTweetsWebService () <NSURLSessionDelegate>
+
+@end
+
 @implementation FetchTweetsWebService
 
 - (void)fetchTweets:(void (^)(NSDictionary *))callback {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         
+        //Configure session headers an oAuth token here
+        //
+        
+        NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        NSString *urlString = [self buildURLString];
+        NSURL *remoteURL = [NSURL URLWithString:urlString];
+        NSURLRequest *request = [[NSURLRequest alloc]  initWithURL:remoteURL];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration delegate:self delegateQueue:nil];
+        
+        [[session dataTaskWithRequest:request completionHandler:
+          ^(NSData *data, NSURLResponse *response, NSError *error) {
+              if (error) {
+                  NSLog(@"error: %@", error.localizedDescription);
+                  callback(nil);
+                  return;
+              }
+              NSError *jsonError = nil;
+              NSDictionary *result = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+              callback(result);
+              NSLog(@"HERE IS THE RESULT: %@", result);
+          }] resume];
     });
 }
 
