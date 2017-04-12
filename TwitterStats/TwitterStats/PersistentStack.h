2d9e535d6c18a7ff81d8c03a093c5462fa3ec6d9
Cameron Weston <cameron.weston@banno.com>
2017-04-12 11:59:14 -0500

    Add core data and xcdatamodel file to project

---
+++ TwitterStats/TwitterStats/PersistentStack.h
@@ -0,0 +1,13 @@
+//
+//  PersistentStack.h
+//  TwitterStats
+//
+//  Created by Cameron Weston on 4/12/17.
+//  Copyright © 2017 cameron weston personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersistentStack : NSObject
+
+@end
---
+++ TwitterStats/TwitterStats/PersistentStack.m
@@ -0,0 +1,13 @@
+//
+//  PersistentStack.m
+//  TwitterStats
+//
+//  Created by Cameron Weston on 4/12/17.
+//  Copyright © 2017 cameron weston personal. All rights reserved.
+//
+
+#import "PersistentStack.h"
+
+@implementation PersistentStack
+
+@end
