ff2bd21b2fef2463a4d9ee1c73454af8417da452
Cameron Weston <cameron.weston@banno.com>
2017-04-12 14:42:14 -0500

    Add implementation for ManagedTweet object

---
+++ TwitterStats/TwitterStats/ManagedTweet.h
@@ -0,0 +1,13 @@
+//
+//  ManagedTweet.h
+//  TwitterStats
+//
+//  Created by Cameron Weston on 4/12/17.
+//  Copyright © 2017 cameron weston personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ManagedTweet : NSObject
+
+@end
---
+++ TwitterStats/TwitterStats/ManagedTweet.m
@@ -0,0 +1,13 @@
+//
+//  ManagedTweet.m
+//  TwitterStats
+//
+//  Created by Cameron Weston on 4/12/17.
+//  Copyright © 2017 cameron weston personal. All rights reserved.
+//
+
+#import "ManagedTweet.h"
+
+@implementation ManagedTweet
+
+@end
