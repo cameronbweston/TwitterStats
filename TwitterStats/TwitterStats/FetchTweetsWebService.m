2905b9f43da95e3c837355772f8ddc5d262e908c
Cameron Weston <cameron.weston@banno.com>
2017-04-11 20:03:05 -0500

    Add Bearer token for Authorization header

--- TwitterStats/TwitterStats/FetchTweetsWebService.m
+++ TwitterStats/TwitterStats/FetchTweetsWebService.m
@@ -14,13 +14,14 @@
 
 @implementation FetchTweetsWebService
 
+static NSString *const token = @"Bearer d61095e0ae01a59d2e7738ed137c18f349b96cbc";
+
 - (void)fetchTweets:(void (^)(NSDictionary *))callback {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        
-        //Configure session headers an oAuth token here
-        //
-        
+
         NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        [sessionConfiguration setHTTPAdditionalHeaders:@{@"Authorization": token}];
+
         NSString *urlString = [self buildURLString];
         NSURL *remoteURL = [NSURL URLWithString:urlString];
         NSURLRequest *request = [[NSURLRequest alloc]  initWithURL:remoteURL];
@@ -37,6 +38,9 @@
               NSDictionary *result = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
               callback(result);
               NSLog(@"HERE IS THE RESULT: %@", result);
+              if (jsonError) {
+                  NSLog(@"Error with json: %@", jsonError.localizedDescription);
+              }
           }] resume];
     });
 }
