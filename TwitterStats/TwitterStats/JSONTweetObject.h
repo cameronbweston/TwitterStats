bf23f252c3d98629078aa45384e0ac60ad9882ce
Cameron Weston <cameron.weston@banno.com>
2017-04-25 11:37:01 -0500

    Update app and add libraries to test targets

---
+++ TwitterStats/TwitterStats/JSONTweetObject.h
@@ -0,0 +1,13 @@
+//
+//  JSONTweetObject.h
+//  TwitterStats
+//
+//  Created by Cameron Weston on 4/25/17.
+//  Copyright © 2017 cameron weston personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONTweetObject : NSObject
+
+@end
---
+++ TwitterStats/TwitterStats/JSONTweetObject.m
@@ -0,0 +1,13 @@
+//
+//  JSONTweetObject.m
+//  TwitterStats
+//
+//  Created by Cameron Weston on 4/25/17.
+//  Copyright © 2017 cameron weston personal. All rights reserved.
+//
+
+#import "JSONTweetObject.h"
+
+@implementation JSONTweetObject
+
+@end
