35f5f24d00f67f52d291c1eec0f9f03f4d04ff15
Cameron Weston <cameron.weston@banno.com>
2017-04-26 19:56:21 -0500

    Add checks for null dictionary keys in jsonTweet

--- TwitterStats/TwitterStats/JSONTweetObject.m
+++ TwitterStats/TwitterStats/JSONTweetObject.m
@@ -18,55 +18,30 @@
 }
 
 - (void)loadFromDictionary:(NSDictionary *)jsonTweet {
-    //array items here for testing
-        [self validateStringValueForKey:@"id_str" fromDicitonary:jsonTweet block:^(NSString *stringValue) {
-            self.tweetID = stringValue;
-        }];
-
+    if ([jsonTweet valueForKey:@"id_str"] != [NSNull null]) {
+        self.tweetID = [jsonTweet valueForKey:@"id_str"];
+    }
+    if ([[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"hashtags"] != [NSNull null]) {
+        self.hashtags = [[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"hashtags"];
+    }
+    if ([[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"symbols"] != [NSNull null]) {
+        self.symbols = [[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"symbols"];
+    }
+    if ([[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"urls"] != [NSNull null]) {
+        self.urls = [[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"urls"];
+    }
+    if ([jsonTweet valueForKeyPath:@"text"] != [NSNull null]) {
+        self.text = [jsonTweet valueForKeyPath:@"text"];
+    }
+    if ([jsonTweet valueForKeyPath:@"created_at"] != [NSNull null]) {
+        self.dateCreated = [jsonTweet valueForKeyPath:@"created_at"];
+    }
         NSArray *item2 = [[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"hashtags"];
         NSArray *item3 = [[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"symbols"];
         NSArray *item4 = [[jsonTweet valueForKeyPath:@"entities"] valueForKeyPath:@"urls"];
         NSString *item5 = [jsonTweet valueForKeyPath:@"text"];
         NSString *item6 = [jsonTweet valueForKeyPath:@"created_at"];
-
-        self.hashtags = item2;
-        self.symbols = item3;
-        self.urls = item4;
-        self.text = [jsonTweet valueForKeyPath:@"text"];
-        self.dateCreated = [jsonTweet valueForKeyPath:@"created_at"];
-}
-
-- (BOOL)validateStringValueForKey:(NSString *)key
-                   fromDicitonary:(NSDictionary *)dict
-                            block:(void (^)(NSString *stringValue))block {
-    __kindof NSObject *const jsonValue = dict[key];
     
-    const BOOL isString = [jsonValue isKindOfClass:[NSString class]];
-    
-    if (isString) {
-        block(jsonValue);
-    }
-    
-    return isString;
-}
-
-- (NSNumber *)validateNumberValueForKey:(NSString *)key
-                         fromDicitonary:(NSDictionary *)dict {
-    __kindof NSObject *const jsonValue = dict[key];
-    NSNumber *resultNumber;
-    
-    if ([jsonValue isKindOfClass:[NSNumber class]]) {
-        resultNumber = jsonValue;
-    }
-    else if ([jsonValue isKindOfClass:[NSString class]]) {
-        NSNumber *const numberValue = [NSDecimalNumber decimalNumberWithString:jsonValue];
-        
-        if (nil != numberValue && ! [numberValue isEqualToNumber:[NSDecimalNumber notANumber]]) {
-            resultNumber = numberValue;
-        }
-    }
-    
-    return resultNumber;
 }
 
 @end
