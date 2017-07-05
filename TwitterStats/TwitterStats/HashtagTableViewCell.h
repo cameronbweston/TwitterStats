e10ca1f0f4cf36e0789ac05214e3a3db5ee9ea2f
Cameron Weston <cameron.weston@banno.com>
2017-07-05 15:31:04 -0500

    Add custom cell for hashtag table view

---
+++ TwitterStats/TwitterStats/HashtagTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HashtagTableViewCell.h
+//  TwitterStats
+//
+//  Created by Cameron Weston on 7/5/17.
+//  Copyright © 2017 cameron weston personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HashtagTableViewCell : UITableViewCell
+
+@end
---
+++ TwitterStats/TwitterStats/HashtagTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  HashtagTableViewCell.m
+//  TwitterStats
+//
+//  Created by Cameron Weston on 7/5/17.
+//  Copyright © 2017 cameron weston personal. All rights reserved.
+//
+
+#import "HashtagTableViewCell.h"
+
+@implementation HashtagTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
